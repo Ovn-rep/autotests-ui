192bfcbb8101851af32c9f33e113f87260b6bbc9
Ovn-rep <alex333karasev@yandex.ru>
2026-01-02 20:55:35 +0300

    progress script

--- playwright_authorization.py
+++ playwright_authorization.py
@@ -7,11 +7,11 @@ with sync_playwright() as playwright:
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    email_input = page.locator('//div[@data-testid="login-form-email-input"]//input')
+    email_input = page.get_by_test_id('login-form-email-input').locator('input')
     email_input.fill('user.name@gmail.com')
-    password_input = page.locator('//div[@data-testid="login-form-password-input"]//input')
+    password_input = page.get_by_test_id('login-form-password-input').locator('input')
     password_input.fill('Password')
-    login_button = page.locator('//button[@data-testid="login-page-login-button"]')
+    login_button = page.get_by_test_id('login-page-login-button')
     login_button.click()
     Wrong_email_or_password_alert = page.locator('//div[@data-testid="login-page-wrong-email-or-password-alert"]')
 
