abf90f3f1009eb321a6c28219c80cb3955aa0638
Ovn-rep <alex333karasev@yandex.ru>
2026-01-10 02:31:25 +0300

    Фикстуры Pytest

--- playwright_courses.py
+++ playwright_courses.py
@@ -19,11 +19,11 @@ with sync_playwright() as playwright:
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-    context.storage_state(path="browser-state.json")
+    context.storage_state(path="cookie.json")
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json")
+    context = browser.new_context(storage_state="cookie.json")
     page = context.new_page()
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
