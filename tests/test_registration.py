35cb7295935733d941935b1567ad43cc7f5f1011
Ovn-rep <alex333karasev@yandex.ru>
2026-01-11 04:58:52 +0300

    Знакомство с PageObject

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,20 +1,11 @@
-from playwright.sync_api import sync_playwright, expect, Page
+from pages.registration_page import RegistrationPage
+from pages.dashboard_page import DashboardPage
+from playwright.sync_api import Page
 
-def test_successfull_registration(chromium_page: Page):
+def test_successfull_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
+        registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-        chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        registration_page.fill_registration_form()
+        registration_page.click_registration_button()
 
-        email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill("email@yande.ru")
-
-        username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill("username")
-
-        password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill("user12345")
-
-        registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        dashboard_title = chromium_page.get_by_test_id("dashboard-toolbar-title-text")
-        expect(dashboard_title).to_be_visible()
+        dashboard_page.check_dashboard_title_to_be_visible()
