3c55604fd5c0a994a118ef1115d142682bd0be63
Ovn-rep <alex333karasev@yandex.ru>
2026-03-28 13:35:10 +0300

    Запуск автотестов на GitHub Actions

--- fixtures/allure.py
+++ fixtures/allure.py
@@ -2,7 +2,7 @@ import pytest
 from tools.allure.environment import create_environment_properties_dir
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="session ", autouse=True)
 def save_allure_environment_file():
     yield
     create_environment_properties_dir()
