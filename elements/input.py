582c69dec51196927070c98a0c793f65e641c651
Ovn-rep <alex333karasev@yandex.ru>
2026-03-30 03:22:54 +0300

    Удаление артефактов покрытия

--- elements/input.py
+++ elements/input.py
@@ -1,4 +1,6 @@
 import allure
+from ui_coverage_tool import ActionType
+
 from elements.base_element import BaseElement
 from playwright.sync_api import expect, Locator
 from tools.playwright.logger import get_logger
@@ -14,6 +16,9 @@ class Input(BaseElement):
     def get_by_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_by_locator(nth, **kwargs).locator('input')
 
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        return f'{super().get_raw_locator(**kwargs)}//input'
+
     def fill(self, text: str, nth: int = 0, **kwargs):
         step = f"Fill {self.type_of} '{self.name}' to value '{text}'"
         with allure.step(step):
@@ -21,9 +26,13 @@ class Input(BaseElement):
             logger.info(step)
             locator.fill(text)
 
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
+
     def check_have_value(self, text: str, nth: int = 0, **kwargs):
         step = f"Checking that {self.type_of} '{self.name}' has a value '{text}'"
         with allure.step(step):
             locator = self.get_by_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_value(text)
+
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
